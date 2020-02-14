833093a3771139a3dfb6e8df8190c04d1be68b79
liv5244 <18851751131@163.com>
2020-02-14 12:31:38 +0800

    first commit_

---
+++ src/main/java/com/itliv/community/SpringbootCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.itliv.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootCommunityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/itliv/community/SpringbootCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itliv.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
