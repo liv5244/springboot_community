cca4d557691cb9409d32e661284d3cb714a83256
liv5244 <18851751131@163.com>
2020-02-14 18:01:48 +0800

    add session in the login module

--- src/main/java/com/itliv/community/controller/OAuthController.java
+++ src/main/java/com/itliv/community/controller/OAuthController.java
@@ -7,9 +7,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @Slf4j
@@ -27,7 +28,8 @@ public class OAuthController {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String state, Model model) {
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -36,7 +38,12 @@ public class OAuthController {
         accessTokenDTO.setRedirect_uri(redirectUri);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
-        model.addAttribute("user", user);
-        return "index";
+        if (user != null) {
+            //用户不为null，判断登录成功
+            request.getSession().setAttribute("user", user);
+            return "redirect:/index";
+        } else {
+            return "redirect:/index";
+    }
     }
 }
